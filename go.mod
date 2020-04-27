bc4dfa9d241ecde327ba1e2c580a56073bb03a80
liupengfeng <1198150498@qq.com>
2020-04-27 08:40:36 +0800

    初始化

---
+++ go.mod
@@ -0,0 +1,11 @@
+module hello_kafka_tailf
+
+go 1.13
+
+require (
+	github.com/Shopify/sarama v1.26.1
+	github.com/hpcloud/tail v1.0.0
+	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
---
+++ tailf/tailf.go
@@ -0,0 +1,41 @@
+package tailf
+
+import (
+	"fmt"
+	"github.com/hpcloud/tail"
+	"time"
+)
+
+func Init() {
+
+}
+
+func run() {
+	fileName := "./my.log"
+	config := tail.Config{
+		ReOpen:    true,
+		Follow:    true,
+		Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
+		MustExist: false,
+		Poll:      true,
+	}
+	tails, err := tail.TailFile(fileName, config)
+	if err != nil {
+		fmt.Println("tail file error:", err)
+		return
+	}
+	var (
+		msg *tail.Line
+		ok  bool
+	)
+	for {
+		msg, ok = <-tails.Lines
+		if !ok {
+			fmt.Printf("tail file close reopen,filename:%s\n", tails.Filename)
+			time.Sleep(time.Second)
+			continue
+		}
+		fmt.Println("line:", msg.Text)
+	}
+
+}
